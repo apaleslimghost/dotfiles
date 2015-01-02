b168353c54e2217dc14fa0418c63dd790341578d
Matt Brennan <mattyb1000@gmail.com>
2015-01-02 12:39:57 +0000

    don't die if afred not running

--- alfred/install.sh
+++ alfred/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x -e
-killall Alfred\ 2
-killall Alfred\ Preferences
+killall Alfred\ 2 || echo "Alfred not running"
+killall Alfred\ Preferences || echo "Alfred Prefs not running"
 cp -f alfredhat.png ~/Applications/Alfred\ 2.app/Contents/Frameworks/Alfred\ Framework.framework/Resources/alfredhat.png
 open elementary-dark.alfredappearance
