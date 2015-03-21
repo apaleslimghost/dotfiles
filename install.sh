e35df832ab22822027fc99a8d3e090127494e6ea
Matt Brennan <mattyb1000@gmail.com>
2015-03-21 11:03:40 +0000

    single folder install

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x -e
 
-# link directories, run submodule-specific installation
-for d in $(ls -d -- */); do
+link_and_install() {
+    local d=$1
     if [ ! -f "$d"nolink ]; then
         ln -snf "$(pwd)/${d%/}" ~/".${d%/}"
     fi
@@ -12,4 +12,13 @@ for d in $(ls -d -- */); do
         ./install.sh
         popd
     fi
-done
+}
+
+# link directories, run submodule-specific installation
+if [ $1 == "" ]; then
+    for d in $(ls -d -- */); do
+        link_and_install "$d"
+    done
+elif [ -d "$1" ]; then
+    link_and_install "$1/"
+fi
