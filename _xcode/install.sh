5463fb9547cead3517e3d44b78b058da2f880fff
Matt Brennan <matt@153.io>
2017-11-01 16:26:07 +0000

    matt

--- _xcode/install.sh
+++ _xcode/install.sh
@@ -3,6 +3,6 @@
 mas install 497799835 # that's Xcode
 
 sudo xcode-select -s /Applications/XCode.app/Contents/Developer
-sude xcodebuild -licence accept
+sudo xcodebuild -licence accept
 
 brew bundle
