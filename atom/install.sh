8a8efd0a4ca9fae38e39634ef9acb922d54a5fe7
Matt Brennan <matt@153.io>
2017-12-05 12:22:24 +0000

    atom improvements

--- atom/install.sh
+++ atom/install.sh
@@ -1,3 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-npm install
+apm='apm'
+
+if command -v apm-beta > /dev/null ; then
+	apm='apm-beta'
+fi
+
+installed_packages() {
+	$apm ls -i -b | cut -d @ -f 1
+}
+
+not_installed=$(comm -23 Atomfile <(installed_packages))
+installed_but_shouldnt_be=$(comm -23 <(installed_packages) Atomfile)
+
+if [ "$not_installed" != "" ] ; then
+	$apm install $not_installed
+else
+	echo "No Atom packages to install"
+fi
+
+
+if [ "$installed_but_shouldnt_be" != "" ] ; then
+	$apm remove $installed_but_shouldnt_be
+else
+	echo "No Atom packages to remove"
+fi
