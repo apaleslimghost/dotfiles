0f11387ed9159bdbc0411165f0f85c8a5be43b4e
Matt Brennan <matt@153.io>
2018-01-25 13:31:49 +0000

    redo things

--- fish/functions/apm.fish
+++
@@ -1,7 +0,0 @@
-function apm
-    if test (command -s apm-beta) != ""
-        apm-beta $argv
-    else
-        apm $argv
-    end
-end
--- fish/functions/atom.fish
+++
@@ -1,7 +0,0 @@
-function atom
-    if test (command -s atom-beta) != ""
-        atom-beta $argv
-    else
-        atom $argv
-    end
-end
