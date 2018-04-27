ad4a75250400b19e78c38e8e7c0ef16aa2f48c13
Matt Brennan <matt@153.io>
2018-04-27 16:38:19 +0100

    lol

--- _brew/install.sh
+++ _brew/install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sudo rm -rf /Applications/Google\ Chrome.app /Applications/Slack.app
+
 if ! command -v brew >/dev/null 2>&1 ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
