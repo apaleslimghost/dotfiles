978da440685566713d789a11e29e8d47caf22320
Matt Brennan <matt@153.io>
2018-04-28 10:49:25 +0100

    only ask for token if it's not in keychain, open github for it

--- vault/install.sh
+++ vault/install.sh
@@ -1,11 +1,14 @@
 #/bin/sh
 
-echo -n "Github OAuth token to auth with Vault: "
-read -s vault_token
+if ! security find-generic-password -a $USER -s "FT Vault" -w 2>&1 > /dev/null ; then
+	open "https://github.com/settings/tokens/new?scopes=read.org&description=Vault+`hostname`"
+	echo -n "Github OAuth token to auth with Vault: "
+	read -s vault_token
 
-security add-generic-password -a "$USER" -s "FT Vault" -w "$vault_token"
+	security add-generic-password -a "$USER" -s "FT Vault" -w "$vault_token"
 
-echo "Added token to Keychain"
+	echo "Added token to Keychain"
+fi
 
 export VAULT_AUTH_GITHUB_TOKEN=$(security find-generic-password -a $USER -s "FT Vault" -w)
 export VAULT_ADDR="https://vault.in.ft.com"
