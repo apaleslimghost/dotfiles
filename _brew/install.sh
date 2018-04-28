a35ea2ace7631ef3311a65780f38970f80711eb8
Matt Brennan <matt@153.io>
2018-04-28 10:54:32 +0100

    cask might not be installed by then

--- _brew/install.sh
+++ _brew/install.sh
@@ -8,11 +8,11 @@ if ! command -v brew >/dev/null 2>&1 ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-if ! brew cask ls | grep google-chrome ; then
+if [ ! -d /usr/local/Caskroom/google-chrome ] ; then
    sudo rm -rf /Applications/Google\ Chrome.app
 fi
 
-if ! brew cask ls | grep slack ; then
+if [ ! -d /usr/local/Caskroom/slack ] ; then
    sudo rm -rf /Applications/Slack.app
 fi
 
