6965b9405a0cc8d30a0f5f66b1df3648d0fc8a1c
Matt Brennan <matt@153.io>
2018-04-28 11:03:33 +0100

    skip some stuff

--- _brew/install.sh
+++ _brew/install.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-
-
 if ! command -v brew >/dev/null 2>&1 ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
--- vault/install.sh
+++ vault/install.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+exit 0
+
 if ! security find-generic-password -a $USER -s "FT Vault" -w 2>&1 > /dev/null ; then
 	open "https://github.com/settings/tokens/new?scopes=read.org&description=Vault+`hostname`"
 	echo -n "Github OAuth token to auth with Vault: "
