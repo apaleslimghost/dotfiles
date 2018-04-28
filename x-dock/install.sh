669dd0adfe6c6c22526482c3371cf84d7befcf23
Matt Brennan <matt@153.io>
2018-04-28 11:06:32 +0100

    things have changed

--- x-dock/install.sh
+++ x-dock/install.sh
@@ -3,14 +3,14 @@
 dockutil --remove all --no-restart
 
 dockutil --add '/Applications/Google Chrome.app' --no-restart
-dockutil --add '/Applications/Airmail 3.app' --no-restart
+dockutil --add '/Applications/Boxy.app' --no-restart
 dockutil --add '/Applications/Slack.app' --no-restart
 dockutil --add '/Applications/Calendar.app' --no-restart
 dockutil --add '/Applications/Notion.app' --no-restart
-dockutil --add '/Applications/Atom Beta.app' --no-restart
-dockutil --add '/Applications/Hyper.app' --no-restart
+dockutil --add '/Applications/Atom.app' --no-restart
+dockutil --add '/Applications/XCode.app' --no-restart
+dockutil --add '/Applications/iTerm.app' --no-restart
 dockutil --add '/Applications/Spotify.app' --no-restart
-dockutil --add '/Applications/App Store.app' --no-restart
 dockutil --add '/Applications/System Preferences.app' --no-restart
 
 dockutil --add '' --type spacer --section apps
