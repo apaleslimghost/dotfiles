3b5d9f6aa48cf39fd996d7f5857cc0298fa4581a
Matt Brennan <matt@153.io>
2018-04-28 10:43:43 +0100

    delete the things only if they're not casked

--- _brew/install.sh
+++ _brew/install.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-sudo rm -rf /Applications/Google\ Chrome.app /Applications/Slack.app
+
 
 if ! command -v brew >/dev/null 2>&1 ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if ! brew cask ls | grep google-chrome ; then
+   sudo rm -rf /Applications/Google\ Chrome.app
+fi
+
+if ! brew cask ls | grep slack ; then
+   sudo rm -rf /Applications/Slack.app
 fi
 
 brew update
