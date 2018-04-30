25cda616c7a57d12185185b1589ec49677786fa1
Matt Brennan <matt@153.io>
2018-04-30 10:59:57 +0100

    installation script improvements

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 set -e
 
 link_and_install() {
-    local d=$1
+    local d=${1%/}
 
     echo "  ⎙ setting up $d"
 
-    if [ ! -f "$d"nolink ]; then
-        local target=".${d%/}"
-        if [ -f "$d"linktarget ]; then
-            target=$(< "$d"linktarget)
+    if [ ! -f "$d"/nolink ]; then
+        local target=".$d"
+        if [ -f "$d"/linktarget ]; then
+            target=$(< "$d"/linktarget)
         fi
 
         echo "  ⎌ linking folder to $target"
 
         mkdir -p ~/"$(dirname "$target")"
-        ln -snf "$(pwd)/${d%/}" ~/"$target"
+        ln -snf "$(pwd)/$d" ~/"$target"
     fi
 
-    if [ -f "$d"install.sh ]; then
+    if [ -f "$d"/install.sh ]; then
         pushd "$d" > /dev/null
         ./install.sh
         popd > /dev/null
@@ -28,11 +28,12 @@ link_and_install() {
     echo ''
 }
 
-# link directories, run submodule-specific installation
+modules=$@
+
 if [ "$1" == "" ]; then
-    for d in $(ls -d -- */); do
-        link_and_install "$d"
-    done
-elif [ -d "$1" ]; then
-    link_and_install "$1/"
+    modules=$(ls -d -- */)
 fi
+
+for d in $modules; do
+    link_and_install "$d"
+done
