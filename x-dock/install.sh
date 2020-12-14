0b70dc242d365eb6b8469d86c4c312de2ba31ce3
Kara Brightwell <kara@153.io>
2020-12-14 10:55:38 +0000

    update dock

--- x-dock/install.sh
+++ x-dock/install.sh
@@ -2,15 +2,15 @@
 
 dockutil --remove all --no-restart
 
-dockutil --add '/Applications/Google Chrome.app' --no-restart
-dockutil --add '/Applications/Boxy.app' --no-restart
+dockutil --add '/Applications/Firefox.app' --no-restart
+dockutil --add '/Applications/Emacs.app' --no-restart
+dockutil --add '/Applications/Jira.app' --no-restart
 dockutil --add '/Applications/Slack.app' --no-restart
+dockutil --add '/Applications/Discord.app' --no-restart
+dockutil --add '/Applications/Spotify.app' --no-restart
+dockutil --add '/Applications/Music.app' --no-restart
 dockutil --add '/Applications/Calendar.app' --no-restart
 dockutil --add '/Applications/Notion.app' --no-restart
-dockutil --add '/Applications/Atom.app' --no-restart
-dockutil --add '/Applications/XCode.app' --no-restart
-dockutil --add '/Applications/iTerm.app' --no-restart
-dockutil --add '/Applications/Spotify.app' --no-restart
-dockutil --add '/Applications/System Preferences.app' --no-restart
+dockutil --add '/Applications/Bear.app' --no-restart
+dockutil --add '/Applications/System Preferences.app'
 
-dockutil --add '' --type spacer --section apps
