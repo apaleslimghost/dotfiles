194a502b245eceefdafbf27280edbb60c0044d1a
Kara Brightwell <kara@153.io>
2020-12-14 14:57:32 +0000

    update brew stuff

--- _brew/install.sh
+++ _brew/install.sh
@@ -3,13 +3,10 @@
 set -e
 
 if ! command -v brew >/dev/null 2>&1 ; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [ ! -d /usr/local/Caskroom/google-chrome ] ; then
-   sudo rm -rf /Applications/Google\ Chrome.app
-fi
-
+# remove FT-provided Slack.app
 if [ ! -d /usr/local/Caskroom/slack ] ; then
    sudo rm -rf /Applications/Slack.app
 fi
