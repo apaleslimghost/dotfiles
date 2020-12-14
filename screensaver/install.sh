4787d8862956a3da59c2453561714ecc1aa99570
Kara Brightwell <kara@153.io>
2020-12-14 10:56:55 +0000

    remove screensaver it doesn't work

--- screensaver/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x -e
-
-ln -snf "$(pwd)/RedPill v12Sierra.saver" "$HOME/Library/Screen Savers/RedPill v12Sierra.saver"
-
-defaults write com.apple.screensaver modulePath -string "$HOME/Library/Screen Savers/RedPill v12Sierra.saver"
--- screensaver/readme.md
+++
@@ -1,3 +0,0 @@
-# i didn't name the screensaver, okay
-
-it's got nothing to do with the MRA/reactionary movement ☹️
