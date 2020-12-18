0cf951698c118176d0c222306017c18d8caa1fc5
Kara Brightwell <kara@153.io>
2020-12-18 12:27:49 +0000

    doom requirements

--- doom/install.sh
+++ doom/install.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 ln -sf /usr/local/opt/emacs-plus@28/Emacs.app /Applications
 
+if ! which grip > /dev/null; then
+	pip3 install grip
+fi
+
 if [ ! -d ~/.emacs.d ]; then
 	git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 else
