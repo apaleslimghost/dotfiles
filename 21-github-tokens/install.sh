fc16575988ecfdde22034b16a1b3b411397d7acc
Kara Brightwell <kara@153.io>
2021-05-11 11:45:10 +0100

    consolidate github token installer into single thing

---
+++ 21-github-tokens/install.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+ask_for_token() {
+	local token_name="$1"
+	if ! security find-generic-password -a $USER -s "$token_name" -w 2>&1 > /dev/null ; then
+		open "https://github.com/settings/tokens/new?scopes=read.org&description=$(echo "$token_name" | tr ' ' '+')+`hostname`"
+		echo -n "Github OAuth token for $token_name: "
+		read -s token
+
+		security add-generic-password -a "$USER" -s "$token_name" -w "$token"
+
+		echo "Added $token_name token to Keychain"
+	fi
+}
+
+ask_for_token "FT Vault"
+ask_for_token "Hub"
