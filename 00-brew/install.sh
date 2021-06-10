09f059f34e7d4331a69812e4a515b72c8683fa84
Kara Brightwell <kara@153.io>
2021-06-10 11:13:54 +0100

    don't delete slack

--- 00-brew/install.sh
+++ 00-brew/install.sh
@@ -6,10 +6,5 @@ if ! command -v brew >/dev/null 2>&1 ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# remove FT-provided Slack.app
-if [ ! -d /usr/local/Caskroom/slack ] ; then
-   sudo rm -rf /Applications/Slack.app
-fi
-
 brew update
 brew bundle
