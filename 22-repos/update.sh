7b65c405e3dcd355ccc2f743b434da948f8a144f
Kara Brightwell <kara@153.io>
2022-02-23 12:26:10 +0000

    update repo lists

--- 22-repos/update.sh
+++ 22-repos/update.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e
+
 digest_repos() {
 	local root="$1"
 	local file=$(basename $root)
 
 	for repo in $(ls -t "$root" | head -20); do
 		pushd "$root/$repo" > /dev/null
+		echo "  → " $repo 1>&2
 		git remote get-url origin
 		popd > /dev/null
 	done > $file
