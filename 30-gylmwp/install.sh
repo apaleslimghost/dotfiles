7c1b86597c08c82937fb43a6f57ab2f6d7301641
Kara Brightwell <kara@153.io>
2026-03-14 12:27:15 +0000

    idempotent glymwp

--- 30-gylmwp/install.sh
+++ 30-gylmwp/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
-cp -fR Gylmwp.bundle "$HOME/Library/Keyboard Layouts"
-sudo cp -fR Gylmwp.bundle "/Library/Keyboard Layouts"
-echo '  ‼ log in and back out before switching to it kthx'
+
+if [ ! -d "$HOME/Library/Keyboard Layouts" ]; then
+	cp -fR Gylmwp.bundle "$HOME/Library/Keyboard Layouts"
+	sudo cp -fR Gylmwp.bundle "/Library/Keyboard Layouts"
+	echo '  ‼ log in and back out before switching to it kthx'
+fi
