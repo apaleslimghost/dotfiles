7ffdefd865dca9d211649d9aa932f67afa02be1a
Kara Brightwell <kara@153.io>
2026-07-05 07:58:20 +0100

    kicad 10

---
+++ kicad/10.0/PyShell_pcbnew_startup.py
@@ -0,0 +1,9 @@
+### DEFAULT STARTUP FILE FOR KiCad Python Shell
+# Enter any Python code you would like to execute when the PCBNEW python shell first runs.
+
+# For example, uncomment the following lines to import the current board
+
+# import pcbnew
+# import eeschema
+# board = pcbnew.GetBoard()
+# sch = eeschema.GetSchematic()
